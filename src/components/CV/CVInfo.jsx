3169ff7d233e92289e287d4de97d07be54de306e
XuanYen <xuanyenbentre@gmail.com>
2021-10-12 19:44:17 +0700

    handle download cv

---
+++ src/components/CV/CVInfo.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+    Page,
+    Text,
+    View,
+    Document,
+    StyleSheet,
+    Image
+} from "@react-pdf/renderer";
+import moment from "moment";
+const styles = StyleSheet.create({
+    page: {
+        backgroundColor: "#ffffff",
+        margin: 10
+    },
+    name: {
+        fontSize: 20,
+        textAlign: 'center'
+    },
+    title: {
+        fontSize: 18,
+        margin: 10,
+        fontWeight: 'bold',
+        textDecoration: 'underline'
+    },
+    generalInformation: {
+        marginBottom: 15
+    },
+    text: {
+        fontSize: 15,
+        marginBottom: 5,
+        marginLeft: 15,
+        marginRight: 15
+    },
+});
+
+export function PdfDocument(props) {
+    const { data } = props;
+    console.log(data)
+    return (
+        <Document>
+            <Page style={styles.page}>
+                <View>
+                    <Text style={styles.name}>{data.name.toUpperCase()}</Text>
+                    <Text style={styles.title}>General Information:</Text>
+                    <View style={styles.generalInformation}>
+                        <Text style={styles.text}>Address: {data.address}</Text>
+                        <Text style={styles.text}>Phone: {data.phone}</Text>
+                        <Text style={styles.text}>Email: {data.email}</Text>
+                        <Text style={styles.text}>Website: {data.website}</Text>
+                    </View>
+                </View>
+                <View>
+                    <Text style={styles.title}>Education:</Text>
+                    {data.education.map(ele => (
+                        <View key={ele.id} style={styles.generalInformation}>
+                            <Text style={styles.text}>School: {ele.school}</Text>
+                            {
+                                (ele.timeStart && ele.timeEnd) ? (<Text style={styles.text}>{`${ele.timeStart} - ${ele.timeEnd}`}</Text>) : null
+                            }
+                            {
+                                (ele.major) ? <Text style={styles.text}>Major: {ele.major}</Text> : null
+                            }
+                        </View>
+                    ))}
+                </View>
+                <View>
+                    <Text style={styles.title}>Experience:</Text>
+                    {data.experience.map(ele => (
+                        <View key={ele.id} style={styles.generalInformation}>
+                            <Text style={styles.text}>{ele.company} - {ele.position}</Text>
+                            {(ele.timeJobStart && ele.timeJobEnd) ? (<Text style={styles.text}>{`${ele.timeJobStart}-${ele.timeJobEnd}`}</Text>) : null}
+                            {ele.description ? <Text style={styles.text}>{ele.description}</Text> : null}
+                        </View>
+                    ))}
+                </View>
+            </Page>
+        </Document>
+    );
+}
--- src/components/CV/index.js
+++ src/components/CV/index.js
@@ -11,7 +11,18 @@ import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import DatePicker from '@mui/lab/DatePicker';
 import moment from 'moment';
 import useDebounce from './useDebounce';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SaveIcon from '@mui/icons-material/Save';
+import LoadingButton from '@mui/lab/LoadingButton';
+import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
+import { PdfDocument } from "./CVInfo";
+import { styled } from '@mui/material/styles';
 function CV() {
+    const [info, setInfo] = useState({});
+    const [show, setHide] = useState(false);
     const [numChildrenEducation, setNumChildrenEducation] = useState(1);
     const [numChildrenExperience, setNumChildrenExperience] = useState(1);
     const [name, setName] = useState('XUAN YEN');
@@ -33,7 +44,6 @@ function CV() {
         timeJobStart: '',
         timeJobEnd: ''
     }])
-    const [value, setValue] = React.useState('05/08/2017');
     const handleAddEducation = (e) => {
         e.preventDefault();
         setNumChildrenEducation(numChildrenEducation + 1);
@@ -44,7 +54,9 @@ function CV() {
     }
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("School", education)
+        let info = { name, address, phone, email, website, education, experience };
+        setInfo(info);
+        setHide(true);
     }
     const educationChildren = [], experienceChildren = [];
     for (var i = 0; i < numChildrenEducation; i += 1) {
@@ -53,66 +65,121 @@ function CV() {
     for (var i = 0; i < numChildrenExperience; i += 1) {
         experienceChildren.push(<ExperienceSection useDebounce={useDebounce} experience={experience} setExperience={setExperience} key={i} number={i} />);
     };
+    const Div = styled('div')(({ theme }) => ({
+        ...theme.typography.button,
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(1),
+    }));
     return (
-        <Box textAlign='left' sx={{ display: 'flex', justifyContent: 'center' }}>
-            <Box component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '33%' },
-                }}
-                noValidate
-                autoComplete="off"
-                onSubmit={handleSubmit}
-            >
-                <Box>
-                    <Typography variant='h6'>General information</Typography>
-                    <LocalizationProvider dateAdapter={AdapterDateFns}>
-                        <DatePicker
-                            label="Basic example"
-                            value={value}
-                            onChange={(newValue) => {
-                                setValue(newValue);
-                            }}
-                            renderInput={(params) => <TextField {...params} />}
-                        />
-                    </LocalizationProvider>
-                    <TextField id="name" value={name} onInput={e => setName(e.target.value)} label="Name" variant="filled" />
-                    <TextField id="address" label="Address" variant="filled" value={address} onInput={e => setAddress(e.target.value)} />
-                    <TextField id="phone" label="Phone" variant="filled" value={phone} onInput={e => setPhone(e.target.value)} />
-                    <TextField id="email" label="Email" variant="filled" value={email} onInput={e => setEmail(e.target.value)} />
-                    <TextField id="website" label="Website" variant="filled" value={website} onInput={e => setWebsite(e.target.value)} />
+        <Box>
+            <Div>{"Build Your New Resume! Make a perfect resume in 2021 and get your dream job using the free resume."}</Div>
+            <Box textAlign='left' sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Box component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '33%' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    onSubmit={handleSubmit}
+                >
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1a-content"
+                            id="panel1a-header"
+                        >
+                            <Typography>Part 1: General Information</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Box>
+                                <TextField id="name" value={name} onInput={e => setName(e.target.value)} label="Name" variant="filled" />
+                                <TextField id="address" label="Address" variant="filled" value={address} onInput={e => setAddress(e.target.value)} />
+                                <TextField id="phone" label="Phone" variant="filled" value={phone} onInput={e => setPhone(e.target.value)} />
+                                <TextField id="email" label="Email" variant="filled" value={email} onInput={e => setEmail(e.target.value)} />
+                                <TextField id="website" label="Website" variant="filled" value={website} onInput={e => setWebsite(e.target.value)} />
+                            </Box>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2a-content"
+                            id="panel2a-header"
+                        >
+                            <Typography>Part 2: Education</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Box>
+                                <Box id="educationSections">
+                                    {educationChildren}
+                                </Box>
+                                <Box onClick={handleAddEducation} variant='span'>
+                                    <Icon color="primary">add_circle</Icon>
+                                    <Typography variant='span'>Add extra</Typography>
+                                </Box>
+                            </Box>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel3a-content"
+                            id="panel3a-header"
+                        >
+                            <Typography>Part 3: Experience</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Box>
+                                <Box id="experienceSections">
+                                    {experienceChildren}
+                                </Box>
+                                <Box onClick={handleAddExperience} variant='span'>
+                                    <Icon color="primary">add_circle</Icon>
+                                    <Typography variant='span'>Add extra</Typography>
+                                </Box>
+                            </Box>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Button type="submit" sx={{ m: 2 }}>Submit</Button>
                 </Box>
-                <Box>
-                    <Typography variant='h6'>Education
-                        <Box onClick={handleAddEducation} variant='span'>
-                            <Icon color="primary">add_circle</Icon>
-                            <Typography variant='span'>Add extra</Typography>
-                        </Box>
-                    </Typography>
-                    <Box id="educationSections">
-                        {educationChildren}
-                    </Box>
-                </Box>
-                <Box>
-                    <Typography variant='h6'>Experience
-                        <Box onClick={handleAddExperience} variant='span'>
-                            <Icon color="primary">add_circle</Icon>
-                            <Typography variant='span'>Add extra</Typography>
-                        </Box>
-                    </Typography>
-                    <Box id="experienceSections">
-                        {experienceChildren}
-                    </Box>
-                </Box>
-                <Button type="submit">Submit</Button>
             </Box>
+            {show && (
+                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
+                    <img width='300px' src='./candidate.png' />
+                    <PDFViewer width='50%'>
+                        <PdfDocument data={info} />
+                    </PDFViewer>
+                    <PDFDownloadLink
+                        document={<PdfDocument data={info} />}
+                        fileName="cv.pdf"
+                        style={{
+                            textDecoration: 'none',
+                            marginLeft: '20px'
+                        }}
+                    >
+                        {({ blob, url, loading, error }) =>
+                            <LoadingButton
+                                color="secondary"
+                                loading={loading}
+                                loadingPosition="start"
+                                startIcon={<SaveIcon />}
+                                variant="contained"
+                            >
+                                Download CV
+                            </LoadingButton>
+                        }
+                    </PDFDownloadLink>
+                </Box>
+            )}
         </Box>
+
     )
 }
 const EducationSection = props => {
     const [school, setSchool] = useState('HoChiMinh University of Technology');
     const [major, setMajor] = useState('Computer Science');
-    const [timeStart, setTimeStart] = useState('08/05/2017');
-    const [timeEnd, setTimeEnd] = useState('11/20/2021');
+    const [timeStart, setTimeStart] = useState('05/08/2017');
+    const [timeEnd, setTimeEnd] = useState('20/11/2021');
     const { education, number, setEducation } = props;
     const debouncedSchool = useDebounce(school, 2000);
     const debouncedMajor = useDebounce(major, 2000);
@@ -147,6 +214,7 @@ const EducationSection = props => {
             <LocalizationProvider dateAdapter={AdapterDateFns}>
                 <DatePicker
                     value={timeStart}
+                    format="dd/MM/yyyy"
                     onChange={(newValue) => {
                         setTimeStart(newValue);
                     }}
@@ -154,6 +222,7 @@ const EducationSection = props => {
                 />
                 <DatePicker
                     value={timeEnd}
+                    format="dd/MM/yyyy"
                     onChange={(newValue) => {
                         setTimeEnd(newValue);
                     }}
@@ -167,23 +236,26 @@ const EducationSection = props => {
 const ExperienceSection = props => {
     const [company, setCompany] = useState('HCMUT');
     const [position, setPosition] = useState('Intern');
-    const [timeJobStart, setTimeJobStart] = useState('08/03/2020');
-    const [timeJobEnd, setTimeJobEnd] = useState('10/03/2021');
+    const [timeJobStart, setTimeJobStart] = useState('03/08/2020');
+    const [timeJobEnd, setTimeJobEnd] = useState('03/10/2021');
+    const [description, setDescription] = useState('I am responsible for developing new product in almost stages. I have worked as BA, Developer and Tester. Additionally, I also learned more soft skills such as problem solving and critical thinking.')
     const { experience, number, setExperience } = props;
     const debouncedCompany = useDebounce(company, 2000);
     const debouncedPosition = useDebounce(position, 2000);
     const debouncedTimeJobStart = useDebounce(timeJobStart, 2000);
     const debouncedTimeJobEnd = useDebounce(timeJobEnd, 2000);
+    const debouncedDescription = useDebounce(description, 2000);
     useEffect(() => {
-        addExperience(debouncedCompany, debouncedPosition, debouncedTimeJobStart, debouncedTimeJobEnd);
-    }, [debouncedCompany, debouncedPosition, debouncedTimeJobStart, debouncedTimeJobEnd])
-    const addExperience = (company, position, timeJobStart, timeJobEnd) => {
+        addExperience(debouncedCompany, debouncedPosition, debouncedTimeJobStart, debouncedTimeJobEnd, debouncedDescription);
+    }, [debouncedCompany, debouncedPosition, debouncedTimeJobStart, debouncedTimeJobEnd, debouncedDescription])
+    const addExperience = (company, position, timeJobStart, timeJobEnd, description) => {
         let updateExperience = {
             id: number,
             company,
             position,
             timeJobStart,
-            timeJobEnd
+            timeJobEnd,
+            description
         };
         let newExperience = [...experience];
         let found = newExperience.findIndex(ele => ele.id === number);
@@ -198,10 +270,11 @@ const ExperienceSection = props => {
         <Box id={`experience${props.number}`} key={props.number}>
             <Typography variant="h6">Experience {props.number + 1}</Typography>
             <TextField value={company} onInput={(e) => setCompany(e.target.value)} label="Company" variant="filled" />
-            <TextField id="position" onInput={(e) => setPosition(e.target.value)} label="Position" variant="filled" />
+            <TextField value={position} onInput={(e) => setPosition(e.target.value)} label="Position" variant="filled" />
             <LocalizationProvider dateAdapter={AdapterDateFns}>
                 <DatePicker
                     value={timeJobStart}
+                    format="dd/MM/yyyy"
                     onChange={(newValue) => {
                         setTimeJobStart(newValue);
                     }}
@@ -209,6 +282,7 @@ const ExperienceSection = props => {
                 />
                 <DatePicker
                     value={timeJobEnd}
+                    format="dd/MM/yyyy"
                     onChange={(newValue) => {
                         setTimeJobEnd(newValue);
                     }}
@@ -216,13 +290,7 @@ const ExperienceSection = props => {
                 />
             </LocalizationProvider>
             <Box sx={{ m: 1 }}>
-                <TextareaAutosize
-                    maxRows={4}
-                    aria-label="maximum height"
-                    placeholder="Maximum 4 rows"
-                    defaultValue="I am responsible for developing new product in almost stages. I have worked as BA, Developer and Tester. Additionally, I also learned more soft skills such as problem solving and critical thinking."
-                    style={{ width: '50%' }}
-                />
+                <TextField value={description} onInput={(e) => setDescription(e.target.value)} style={{ width: '66%' }} multiline rows={5} rowsMax={5} label="Description" variant="filled" />
             </Box>
         </Box>
     )
